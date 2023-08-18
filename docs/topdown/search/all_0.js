ea7e387a674851b3be3c8074cc142e314ac872c3
Ian Parberry <Ian-Parberry@users.noreply.github.com>
2023-08-18 13:53:23 -0500

    Documentation.

--- docs/topdown/search/all_0.js
+++ docs/topdown/search/all_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['armedobject_2eh_0',['ArmedObject.h',['../_armed_object_8h.html',1,'']]],
-  ['atworldedge_1',['AtWorldEdge',['../class_c_object_manager.html#a8fd3f8660bf6e9c30a681453d776c111',1,'CObjectManager']]]
+  ['armedobject_2ecpp_0',['ArmedObject.cpp',['../_armed_object_8cpp.html',1,'']]],
+  ['armedobject_2eh_1',['ArmedObject.h',['../_armed_object_8h.html',1,'']]],
+  ['atworldedge_2',['AtWorldEdge',['../class_c_object_manager.html#a8fd3f8660bf6e9c30a681453d776c111',1,'CObjectManager']]]
 ];
--- docs/topdown/search/files_0.js
+++ docs/topdown/search/files_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['armedobject_2eh_0',['ArmedObject.h',['../_armed_object_8h.html',1,'']]]
+  ['armedobject_2ecpp_0',['ArmedObject.cpp',['../_armed_object_8cpp.html',1,'']]],
+  ['armedobject_2eh_1',['ArmedObject.h',['../_armed_object_8h.html',1,'']]]
 ];
