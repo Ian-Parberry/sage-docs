011ff8759e3a9eb26cf5b0db75ef7e0125f99410
Parberry, Ian <Ian.Parberry@unt.edu>
2023-09-05 15:43:59 -0500

    Documentation.

--- docs/topdowntiled/search/functions_8.js
+++ docs/topdowntiled/search/functions_8.js
@@ -1,6 +1,9 @@
 var searchData=
 [
   ['makeboundingboxes_0',['MakeBoundingBoxes',['../class_c_tile_manager.html#a3021ca6fb30b2e041cb63bef80a72feb',1,'CTileManager']]],
-  ['mousehandler_1',['MouseHandler',['../class_c_game.html#a6978045dfc517814ff1a5674507cc56f',1,'CGame']]],
-  ['move_2',['Move',['../class_c_guard.html#a2d1a319a8c34c47a065ca82390f2b279',1,'CGuard::Move()'],['../class_c_object.html#ae33d05b96b8b11fe621e42525a908041',1,'CObject::Move()'],['../class_c_player.html#a8cdce2b8fd7f5765c19d1d45f9899866',1,'CPlayer::Move()']]]
+  ['makeboundingboxes1_1',['MakeBoundingBoxes1',['../class_c_tile_manager.html#a3cae6688a52116f4c86778fd776e9f23',1,'CTileManager']]],
+  ['makeboundingboxesh_2',['MakeBoundingBoxesH',['../class_c_tile_manager.html#a16aecc34eca5babed8212a6c721bd364',1,'CTileManager']]],
+  ['makeboundingboxesv_3',['MakeBoundingBoxesV',['../class_c_tile_manager.html#ab3fe8fe47bbd2f98ecfa6cf75753beb4',1,'CTileManager']]],
+  ['mousehandler_4',['MouseHandler',['../class_c_game.html#a6978045dfc517814ff1a5674507cc56f',1,'CGame']]],
+  ['move_5',['Move',['../class_c_guard.html#a2d1a319a8c34c47a065ca82390f2b279',1,'CGuard::Move()'],['../class_c_object.html#ae33d05b96b8b11fe621e42525a908041',1,'CObject::Move()'],['../class_c_player.html#a8cdce2b8fd7f5765c19d1d45f9899866',1,'CPlayer::Move()']]]
 ];
