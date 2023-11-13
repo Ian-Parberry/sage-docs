985700e496a9fea4e024e79038dc42bd08d8d9bb
Parberry, Ian <Ian.Parberry@unt.edu>
2023-11-13 15:02:35 -0600

    Documentation.

--- docs/ned/search/all_4.js
+++ docs/ned/search/all_4.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['egamestate_0',['eGameState',['../_game_defines_8h.html#ae12dcfc1b1e01980b11e116e3af6807f',1,'GameDefines.h']]],
-  ['emitsmoke_1',['EmitSmoke',['../class_c_plane_object.html#a38b55f8df620a8f5ec1341bfe45d9270',1,'CPlaneObject']]],
-  ['esound_2',['eSound',['../_game_defines_8h.html#a05551407c3195664c546b61dea02e822',1,'GameDefines.h']]],
-  ['esprite_3',['eSprite',['../_game_defines_8h.html#ae419e2bc55ebc731cfdbf4ab769f44e4',1,'GameDefines.h']]],
-  ['estate_4',['eState',['../class_c_crow_object.html#a06a2fa01a55b961eabad1d2f50324c1c',1,'CCrowObject']]]
+  ['edrawmode_0',['eDrawMode',['../_game_defines_8h.html#a9eee2982f33e8c95b34d3acfbc807c77',1,'GameDefines.h']]],
+  ['egamestate_1',['eGameState',['../_game_defines_8h.html#ae12dcfc1b1e01980b11e116e3af6807f',1,'GameDefines.h']]],
+  ['emitsmoke_2',['EmitSmoke',['../class_c_plane_object.html#a38b55f8df620a8f5ec1341bfe45d9270',1,'CPlaneObject']]],
+  ['esound_3',['eSound',['../_game_defines_8h.html#a05551407c3195664c546b61dea02e822',1,'GameDefines.h']]],
+  ['esprite_4',['eSprite',['../_game_defines_8h.html#ae419e2bc55ebc731cfdbf4ab769f44e4',1,'GameDefines.h']]],
+  ['estate_5',['eState',['../class_c_crow_object.html#a06a2fa01a55b961eabad1d2f50324c1c',1,'CCrowObject']]]
 ];
--- docs/ned/search/enums_0.js
+++ docs/ned/search/enums_0.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['egamestate_0',['eGameState',['../_game_defines_8h.html#ae12dcfc1b1e01980b11e116e3af6807f',1,'GameDefines.h']]],
-  ['esound_1',['eSound',['../_game_defines_8h.html#a05551407c3195664c546b61dea02e822',1,'GameDefines.h']]],
-  ['esprite_2',['eSprite',['../_game_defines_8h.html#ae419e2bc55ebc731cfdbf4ab769f44e4',1,'GameDefines.h']]],
-  ['estate_3',['eState',['../class_c_crow_object.html#a06a2fa01a55b961eabad1d2f50324c1c',1,'CCrowObject']]]
+  ['edrawmode_0',['eDrawMode',['../_game_defines_8h.html#a9eee2982f33e8c95b34d3acfbc807c77',1,'GameDefines.h']]],
+  ['egamestate_1',['eGameState',['../_game_defines_8h.html#ae12dcfc1b1e01980b11e116e3af6807f',1,'GameDefines.h']]],
+  ['esound_2',['eSound',['../_game_defines_8h.html#a05551407c3195664c546b61dea02e822',1,'GameDefines.h']]],
+  ['esprite_3',['eSprite',['../_game_defines_8h.html#ae419e2bc55ebc731cfdbf4ab769f44e4',1,'GameDefines.h']]],
+  ['estate_4',['eState',['../class_c_crow_object.html#a06a2fa01a55b961eabad1d2f50324c1c',1,'CCrowObject']]]
 ];
