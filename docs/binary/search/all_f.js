e5781679ea85b38ddc1cf3d0e932df42d76c8eef
Ian Parberry <Ian-Parberry@users.noreply.github.com>
2024-02-19 15:44:26 -0600

    Documentation.
    Set up headings and stubs for everything past the Pinball Game.

--- docs/binary/search/all_f.js
+++ docs/binary/search/all_f.js
@@ -4,5 +4,5 @@ var searchData=
   ['renderer_2ecpp_1',['Renderer.cpp',['../_renderer_8cpp.html',1,'']]],
   ['renderer_2eh_2',['Renderer.h',['../_renderer_8h.html',1,'']]],
   ['renderframe_3',['RenderFrame',['../class_c_game.html#a8b7924e5be62a511e2fc90cc40b7cbb0',1,'CGame']]],
-  ['rw2pw_4',['RW2PW',['../_game_defines_8h.html#a61ee6d8999d820331d7158308e393bbf',1,'RW2PW(float x):&#160;GameDefines.h'],['../_game_defines_8h.html#a26d3c658ea31ebc061926ef3e4e79b5c',1,'RW2PW(int x):&#160;GameDefines.h'],['../_game_defines_8h.html#aa1d20efd7786decd73dbaa858b1228dd',1,'RW2PW(unsigned x):&#160;GameDefines.h'],['../_game_defines_8h.html#a2bc81de2fd51033eefca1e363d23f6ea',1,'RW2PW(int x, int y):&#160;GameDefines.h'],['../_game_defines_8h.html#a3a0abe52d4421b76e7708eb333442a3f',1,'RW2PW(const Vector2 &amp;v):&#160;GameDefines.h']]]
+  ['rw2pw_4',['RW2PW',['../_game_defines_8h.html#a61ee6d8999d820331d7158308e393bbf',1,'RW2PW(float x):&#160;GameDefines.h'],['../_game_defines_8h.html#a26d3c658ea31ebc061926ef3e4e79b5c',1,'RW2PW(int x):&#160;GameDefines.h'],['../_game_defines_8h.html#a2bc81de2fd51033eefca1e363d23f6ea',1,'RW2PW(int x, int y):&#160;GameDefines.h'],['../_game_defines_8h.html#a3a0abe52d4421b76e7708eb333442a3f',1,'RW2PW(const Vector2 &amp;v):&#160;GameDefines.h']]]
 ];
--- docs/lullaby/search/all_e.js
+++ docs/lullaby/search/all_e.js
@@ -3,5 +3,5 @@ var searchData=
   ['release_0',['Release',['../class_c_game.html#aa4cbb6823e28b0b35cd4cac5f17ac797',1,'CGame']]],
   ['removedeadballs_1',['RemoveDeadBalls',['../class_c_object_manager.html#afe2e4eb7f26ed35615d6158c47203827',1,'CObjectManager']]],
   ['renderframe_2',['RenderFrame',['../class_c_game.html#a8b7924e5be62a511e2fc90cc40b7cbb0',1,'CGame']]],
-  ['rw2pw_3',['RW2PW',['../_game_defines_8h.html#a61ee6d8999d820331d7158308e393bbf',1,'RW2PW(float x):&#160;GameDefines.h'],['../_game_defines_8h.html#a26d3c658ea31ebc061926ef3e4e79b5c',1,'RW2PW(int x):&#160;GameDefines.h'],['../_game_defines_8h.html#aa1d20efd7786decd73dbaa858b1228dd',1,'RW2PW(unsigned x):&#160;GameDefines.h'],['../_game_defines_8h.html#a2bc81de2fd51033eefca1e363d23f6ea',1,'RW2PW(int x, int y):&#160;GameDefines.h'],['../_game_defines_8h.html#a3a0abe52d4421b76e7708eb333442a3f',1,'RW2PW(const Vector2 &amp;v):&#160;GameDefines.h']]]
+  ['rw2pw_3',['RW2PW',['../_game_defines_8h.html#a61ee6d8999d820331d7158308e393bbf',1,'RW2PW(float x):&#160;GameDefines.h'],['../_game_defines_8h.html#a26d3c658ea31ebc061926ef3e4e79b5c',1,'RW2PW(int x):&#160;GameDefines.h'],['../_game_defines_8h.html#a2bc81de2fd51033eefca1e363d23f6ea',1,'RW2PW(int x, int y):&#160;GameDefines.h'],['../_game_defines_8h.html#a3a0abe52d4421b76e7708eb333442a3f',1,'RW2PW(const Vector2 &amp;v):&#160;GameDefines.h']]]
 ];
