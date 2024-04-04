a6c1cee9838f8e44cfcce66a4373875628417ac6
Ian Parberry <Ian-Parberry@users.noreply.github.com>
2024-04-03 23:39:50 -0500

    Documentation

--- docs/box2djoints/search/all_3.js
+++ docs/box2djoints/search/all_3.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['default_5fspeed_0',['DEFAULT_SPEED',['../_car_8cpp.html#a25bc1f93aaaaa7a46f58fa4c0980df16',1,'Car.cpp']]],
   ['down_1',['DOWN',['../_nautilus_gear_8cpp.html#a434e4725f04fa366ed1341f04d38521d',1,'NautilusGear.cpp']]],
-  ['draw_2',['draw',['../class_c_sir_isaac_newton.html#a71340497fd4c0bef0c87f89436c23888',1,'CSirIsaacNewton']]],
-  ['draw_3',['Draw',['../class_c_gear.html#a6c8c32fbbcb9aa6e680e5c7494ce5cd2',1,'CGear::Draw()'],['../class_c_level.html#a3e9208196a9e58b60f70dc1da3e7da55',1,'CLevel::Draw()'],['../class_c_line_object.html#a593b11f7a8cc9f9a25b97ac7d2f74d62',1,'CLineObject::Draw()'],['../class_c_newtons_cradle.html#a49d7185f4e7e9e0f778af04c31067436',1,'CNewtonsCradle::Draw()'],['../class_c_object.html#a42e31c3eec1548565a070bbcef6f0c7e',1,'CObject::Draw()']]]
+  ['draw_2',['Draw',['../class_c_level.html#a3e9208196a9e58b60f70dc1da3e7da55',1,'CLevel::Draw()'],['../class_c_line_object.html#a593b11f7a8cc9f9a25b97ac7d2f74d62',1,'CLineObject::Draw()'],['../class_c_newtons_cradle.html#a49d7185f4e7e9e0f778af04c31067436',1,'CNewtonsCradle::Draw()'],['../class_c_object.html#a42e31c3eec1548565a070bbcef6f0c7e',1,'CObject::Draw()']]],
+  ['draw_3',['draw',['../class_c_sir_isaac_newton.html#a71340497fd4c0bef0c87f89436c23888',1,'CSirIsaacNewton']]]
 ];
--- docs/box2djoints/search/functions_3.js
+++ docs/box2djoints/search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['draw_0',['draw',['../class_c_sir_isaac_newton.html#a71340497fd4c0bef0c87f89436c23888',1,'CSirIsaacNewton']]],
-  ['draw_1',['Draw',['../class_c_gear.html#a6c8c32fbbcb9aa6e680e5c7494ce5cd2',1,'CGear::Draw()'],['../class_c_level.html#a3e9208196a9e58b60f70dc1da3e7da55',1,'CLevel::Draw()'],['../class_c_line_object.html#a593b11f7a8cc9f9a25b97ac7d2f74d62',1,'CLineObject::Draw()'],['../class_c_newtons_cradle.html#a49d7185f4e7e9e0f778af04c31067436',1,'CNewtonsCradle::Draw()'],['../class_c_object.html#a42e31c3eec1548565a070bbcef6f0c7e',1,'CObject::Draw()']]]
+  ['draw_0',['Draw',['../class_c_level.html#a3e9208196a9e58b60f70dc1da3e7da55',1,'CLevel::Draw()'],['../class_c_line_object.html#a593b11f7a8cc9f9a25b97ac7d2f74d62',1,'CLineObject::Draw()'],['../class_c_newtons_cradle.html#a49d7185f4e7e9e0f778af04c31067436',1,'CNewtonsCradle::Draw()'],['../class_c_object.html#a42e31c3eec1548565a070bbcef6f0c7e',1,'CObject::Draw()']]],
+  ['draw_1',['draw',['../class_c_sir_isaac_newton.html#a71340497fd4c0bef0c87f89436c23888',1,'CSirIsaacNewton']]]
 ];
