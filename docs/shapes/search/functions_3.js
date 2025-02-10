b3a783d959980efcdcec4effe1563d803848cccc
Ian Parberry <Ian-Parberry@users.noreply.github.com>
2025-02-10 16:08:11 -0600

    Pool & pinball problems

--- docs/shapes/search/functions_3.js
+++ docs/shapes/search/functions_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['intersect_0',['Intersect',['../class_shapes_1_1_c_line.html#aa8ec141c06ee78d7d3c54b560692cc20',1,'Shapes::CLine']]]
+  ['intersect_0',['Intersect',['../class_shapes_1_1_c_line.html#aa8ec141c06ee78d7d3c54b560692cc20',1,'Shapes::CLine']]],
+  ['ispartof_1',['IsPartOf',['../class_shapes_1_1_c_compound_shape.html#abef3eafc01c529d2d9618be7c3fc604e',1,'Shapes::CCompoundShape']]]
 ];
