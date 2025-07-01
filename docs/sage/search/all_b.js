33d68058c306c21987b074cbcccb95bb4b9c6cb2
Ian Parberry <Ian-Parberry@users.noreply.github.com>
2025-07-01 09:59:24 -0500

    3D shape problems.

--- docs/sage/search/all_b.js
+++ docs/sage/search/all_b.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['narrowphase_0',['NarrowPhase',['../class_sage_1_1_c_object_manager.html#a62dc588e37ace87e66df461ebb955233',1,'Sage::CObjectManager']]],
-  ['normalize_1',['Normalize',['../_sage_geometry_8h.html#ab0cb841e0b82c677caed0d07c05b1142',1,'Sage::Normalize(float &amp;)'],['../_sage_geometry_8h.html#ac2bcc3fc8fbb4c793036296dfb101a7b',1,'Sage::Normalize(Vector2)']]],
+  ['normalize_1',['Normalize',['../_sage_geometry_8h.html#ab0cb841e0b82c677caed0d07c05b1142',1,'Sage::Normalize(float &amp;)'],['../_sage_geometry_8h.html#afe4f2c9bd138b2f60d5ae9f826bcfb82',1,'Sage::Normalize(t v)']]],
   ['normalize_2',['normalize',['../_sage_controller_8cpp.html#a12f716c089669df219ba7e8c708caa5c',1,'SageController.cpp']]],
   ['notitle_3',['notitle',['../index.html',1,'']]],
   ['numkeys_4',['NUMKEYS',['../class_sage_1_1_c_keyboard.html#ad755ef691e34ca7f52400b2fa0c98a94',1,'Sage::CKeyboard']]]
