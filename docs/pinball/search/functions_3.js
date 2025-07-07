d2d2ddffa05c839f0e706789d1fb2825af24706b
Ian Parberry <Ian-Parberry@users.noreply.github.com>
2025-07-07 16:22:14 -0500

    Update

--- docs/pinball/search/functions_3.js
+++ docs/pinball/search/functions_3.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['draw_0',['Draw',['../class_c_object_manager.html#a849420297e5c9bf08f073d90b448daf8',1,'CObjectManager']]],
-  ['drawoutline_1',['DrawOutline',['../class_c_object.html#aa45b99376582639dedfc612ab1c29f37',1,'CObject']]],
-  ['drawoutlines_2',['DrawOutlines',['../class_c_object_manager.html#a540f9609ea9a116178aec2c9a43a338a',1,'CObjectManager']]]
+  ['drawframeratetext_1',['DrawFrameRateText',['../class_c_game.html#ad55e21c71b4cef261a09fca0520012f5',1,'CGame']]],
+  ['drawoutline_2',['DrawOutline',['../class_c_object.html#aa45b99376582639dedfc612ab1c29f37',1,'CObject']]],
+  ['drawoutlines_3',['DrawOutlines',['../class_c_object_manager.html#a540f9609ea9a116178aec2c9a43a338a',1,'CObjectManager']]]
 ];
